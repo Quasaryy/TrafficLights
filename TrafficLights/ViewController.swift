bb5796755aa736ec305e7747362f9f8a3f674db2
Yury <quasaryy@gmail.com>
2022-12-28 04:02:19 +0400

    With red on start

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = 10
+        greenView.layer.cornerRadius = 10
+        yellowView.layer.cornerRadius = 10
+        startButton.layer.cornerRadius = 5
+        redView.alpha = 1.0
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+        
     }
 
-
+    @IBAction func startButtonAction() {
+        if redView.alpha == 1.0 {
+            redView.alpha = 0.3
+            yellowView.alpha = 1.0
+            startButton.setTitle("Next", for: .normal)
+        } else if yellowView.alpha == 1.0 {
+            yellowView.alpha = 0.3
+            greenView.alpha = 1.0
+        } else if greenView.alpha == 1.0 {
+            greenView.alpha = 0.3
+            redView.alpha = 1.0
+        }
+    }
+    
 }
 
